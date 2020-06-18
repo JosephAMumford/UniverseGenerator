560bd3fce01be19da31583ff8c4632f305c09749
Josephamumford <josephamumford@gmail.com>
2020-06-17 23:37:51 -0500

    add express

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./static")));
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+
+ res.render("index");
+})
+
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+});
