ad643526b173f1810c56ba2028fa79a949783e0d
Josephamumford <josephamumford@gmail.com>
2020-06-24 21:23:33 -0500

    update

--- static/utilities.js
+++ static/utilities.js
@@ -1,3 +1,20 @@
+class RandomGenerator {
+	constructor(seed) {
+		this.seed = seed;
+		this.rng = new Math.seedrandom(seed);
+	}
+
+	getRandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.ceil(max);
+		return Math.floor(this.rng() * (max - min)) + min;
+	}
+
+	getRandomFloat(min, max) {
+		return this.rng() * (max - min) + min;
+	}
+}
+
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max + 1);
@@ -11,3 +28,7 @@ function getRandomFloat(min, max) {
 String.prototype.capitalize = function () {
 	return this.charAt(0).toUpperCase() + this.slice(1);
 };
+
+function mapValueToRange(value, fromMin, fromMax, toMin, toMax) {
+	return ((value - fromMin) * (toMax - toMin)) / (fromMax - fromMin + toMin);
+}
